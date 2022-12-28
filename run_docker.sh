73be1cac74082e942461e1cb80dbe9188c9e4e83
gusti-ott <g.ottrubay@gmail.com>
2022-12-28 19:04:58 +0100

    update nginx config

--- run_docker.sh
+++ run_docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "killing old docker processes"
-docker-compose rm -fs
+sudo docker compose rm -fs
 
 echo "building docker containers"
-docker-compose up --build -d
+sudo docker compose up --build -d
