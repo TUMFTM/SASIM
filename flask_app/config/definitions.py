386e5698ba1060bda93cdb650dac11ec1e6e5d60
gusti-ott <g.ottrubay@gmail.com>
2023-02-10 12:43:47 +0100

    removing redundant root directories

--- flask_app/config/definitions.py
+++ flask_app/config/definitions.py
@@ -5,12 +5,5 @@ import os
 # ROOT_DIR = os.path.realpath(os.path.join(os.getcwd(), '..'))
 
 # VARIANT 2:
-# root directory for "run"
-# ROOT_DIR = os.path.realpath(os.path.join(os.getcwd(), '../../..'))
-# ROOT_DIR = os.path.realpath(os.path.join(os.getcwd(), '..'))
-
-
-
-# VARIANT 3:
 # root directory for heroku deployment
 ROOT_DIR = os.path.realpath(os.path.join(os.getcwd(), '../app'))
